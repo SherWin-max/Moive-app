4fbcfab755044ec584fa99cefe7c02ab3be5bb4e
Shervin Rahmanian <shervin.rahmanian@yahoo.com>
2023-01-17 10:14:53 -0500

    Moive app

---
+++ js/app.js
@@ -0,0 +1,76 @@
+
+
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=a0814a81d9e0ea8e164320078c18b3cb&query="';
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+
+
+
+async function getMovie(milad) {
+    const res = await fetch('https://api.themoviedb.org/3/movie/' + milad + '?api_key=b535289434a1f9156c6afbfac0f77373');
+    const data = await res.json();
+
+    showMovies(data)
+}
+for (let index = 500; index < 1000; index++) {
+   getMovie(index)
+}
+
+
+
+function showMovies(movies) {
+    // main.innerHTML = '';
+
+
+    const { title, poster_path, vote_average, overview } = movies
+
+    const movieEl = document.createElement('div')
+    movieEl.classList.add('movie')
+
+    movieEl.innerHTML = `
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        `
+
+    main.appendChild(movieEl)
+
+}
+
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return 'green'
+    } else if (vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value
+
+    if (searchTerm && searchTerm !== '') {
+        getMovie(SEARCH_API + searchTerm)
+
+        search.value = ''
+    } else {
+        window.location.reload()
+    }
+})
